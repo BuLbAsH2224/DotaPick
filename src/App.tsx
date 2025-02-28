83ba938f3e088c274d2f30707a434ff983106c77
BuLbAsH2224 <bulbash120@gmail.com>
2025-02-28 22:46:41 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,15 @@
-import { useEffect, useState } from "react";
-import { getHeroStatsAPI } from "./api";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { I_HeroStats } from "./types";
 import { DefaultHeader, DefaultFooter } from "./components";
 import { MainPage } from "./pages";
 
 function App() {
-  const [heroStats, setHeroStats] = useState<I_HeroStats[]>([]);
-  useEffect(() => {
-    const getData = async () => {
-      const data = await getHeroStatsAPI();
-      setHeroStats(data);
-    };
-    getData();
-  }, []);
-  useEffect(() => {
-    console.log(heroStats);
-  }, [heroStats]);
   return (
     <>
       <DefaultHeader />
       <div className="mainContent">
         <Routes>
-        <Route path="/" element={<MainPage />} />
+          <Route path="/" element={<MainPage />} />
         </Routes>
       </div>
       <DefaultFooter />
